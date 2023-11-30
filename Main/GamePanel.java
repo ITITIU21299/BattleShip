976d5346f3f3059d96d99877b2ecf6e95a3d27aa
ITITIU21299 <matofottv@gmail.com>
2023-11-30 23:33:37 +0700

    trying to draw img

--- Main/GamePanel.java
+++ Main/GamePanel.java
@@ -1,11 +1,41 @@
 package Main;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 import Inputs.KeyboardInputs;
+import Inputs.MouseInputs;
+
 
 public class GamePanel extends JPanel {
+    private MouseInputs mouseInputs;
+    private BufferedImage img;
     public GamePanel(){
+
+        importImg();
+
+        mouseInputs = new MouseInputs();
+
         addKeyListener(new KeyboardInputs());
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
+    }
+
+    private void importImg() {
+        InputStream is = getClass().getResourceAsStream("/MainMenu.jpg");
+        try {
+            img = ImageIO.read(is);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.drawImage(img, 0, 0, null);
     }
 }
